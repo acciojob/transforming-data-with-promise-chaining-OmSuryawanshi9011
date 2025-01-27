037aa328ed83640601223712170eeb02b8a03dd9
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-01-27 14:59:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -37,43 +37,4 @@ function startPromiseChain() {
     .catch(error => {
         console.error('An error occurred:', error);
     });
-}const btn = document.getElementById('btn');
-btn.addEventListener('click', startPromiseChain);
-
-function startPromiseChain() {
-    const inputNumber = Number(document.getElementById('ip').value);
-    new Promise((resolve, reject) => {
-        setTimeout(() => resolve(inputNumber), 2000);
-    })
-    .then(result => {
-        result *= 2;
-        document.getElementById('output').innerText = `Result: ${result}`;
-        return new Promise((resolve, reject) => {
-            setTimeout(() => resolve(result), 1000);
-        });
-    })
-    .then(result => {
-        result -= 3;
-        document.getElementById('output').innerText = `Result: ${result}`;
-        return new Promise((resolve, reject) => {
-            setTimeout(() => resolve(result), 1000);
-        });
-    })
-    .then(result => {
-        result /= 2;
-        document.getElementById('output').innerText = `Result: ${result}`;
-        return new Promise((resolve, reject) => {
-            setTimeout(() => resolve(result), 1000);
-        });
-    })
-    .then(result => {
-        result += 10;
-        document.getElementById('output').innerText = `Final Result: ${result}`;
-        return new Promise((resolve, reject) => {
-            setTimeout(() => resolve(result), 1000);
-        });
-    })
-    .catch(error => {
-        console.error('An error occurred:', error);
-    });
 }
